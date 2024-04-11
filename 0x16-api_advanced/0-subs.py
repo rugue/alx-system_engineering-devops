cf7fc5fd95f7e0319842465ffa7938cd53f2fb61
Osarugue Enehizena <nehirugue@gmail.com>
2024-04-11 02:12:19 -0700

    done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
-'''
-    this module contains the function number_of_subscribers
-'''
-import requests
-from sys import argv
+"""
+number of subscribers for a given subreddit
+"""
+
+from requests import get
 
 
 def number_of_subscribers(subreddit):
-    '''
-        returns the number of subscribers for a given subreddit
-    '''
-    user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/about.json'
-                       .format(subreddit), headers=user).json()
-    try:
-        return url.get('data').get('subscribers')
-    except Exception:
+    """
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
 
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
 
-if __name__ == "__main__":
-    number_of_subscribers(argv[1])
+    try:
+        return results.get('data').get('subscribers')
+
+    except Exception:
+        return 0
