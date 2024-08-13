1e4e9f1e0ce11bd2156f8fc58e1d9ae69fe4eff6
Enehizena Osarugue <nehirugue@gmail.com>
2024-08-13 22:42:37 +0100

    Updated the 0-subs.py file agian

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,20 +7,14 @@ If an invalid subreddit is given, the function should return 0.
 
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """
-    Function that queries the Reddit API
-    - If not a valid subreddit, return 0.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "my-app/0.0.1"}
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            return response.json().get("data", {}).get("subscribers", 0)
-        else:
-            return 0
-    except requests.RequestException:
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
